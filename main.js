7759a6ede72fc8189b301cae889a82eaab1041f2
Jelmer84 <74977931+Jelmer84@users.noreply.github.com>
2021-02-10 17:34:15 +0100

    les klaar!

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
+async function displayPrizes () {
+    const response = await axios.get("https://ignoble-api.herokuapp.com/prizes?year=2000"
+    );
+    // console.log(response.data.prizes)
+    const prizelist = document.getElementById("prize-list")
+    // console.log(prizelist)
 
-console.log(axios.get)
+    for (const prize of response.data.prizes) {
+        // console.log("Prize", prize)
+        const listItem = document.createElement("li")
+
+        listItem.textContent = ` ${prize.year} - ${prize.type} ${prize.description}`;
+        // console.log(listItem)
+
+        prizelist.appendChild(listItem)
+    }
+}
+
+displayPrizes();
